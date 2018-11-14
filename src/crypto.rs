1e754d4c1cabf86fe86be6f6432e55947cf15b6a
Terkwood <metaterkhorn@gmail.com>
2018-11-14 18:46:33 -0500

    restore the world

--- src/crypto.rs
+++ src/crypto.rs
@@ -103,7 +103,7 @@ fn verify_rsa(
     signing_input: &str,
     key: &[u8],
 ) -> Result<bool> {
-    let signature_bytes = base64::decode_config(signature, base64::URL_SAFE)?;
+    let signature_bytes = base64::decode_config(signature, base64::URL_SAFE_NO_PAD)?;
     println!("We here 0");
     let public_key_der = untrusted::Input::from(key);
     let message = untrusted::Input::from(signing_input.as_bytes());
